78e2ac763e09960cff5371562a85326037cae508
Shaik Edulla Malik <smalik@altimetrik.com>
2021-06-11 18:02:11 +0530

    second commit

--- inspired/src/com/inheritance/example/Animal.java
+++ inspired/src/com/inheritance/example/Animal.java
@@ -1,4 +1,8 @@
 package com.inheritance.example;
 public class Animal {
 
+    public Animal(){
+        System.out.println("In animal class");
+    }
+
 }
---
+++ inspired/src/com/inheritance/example/Testing.java
@@ -0,0 +1,7 @@
+package com.inheritance.example;
+
+public class Testing {
+    public static void main(String[] args) {
+        Animal a= new Animal();
+    }
+}
